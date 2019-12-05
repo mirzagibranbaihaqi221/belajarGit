d69187bdd98946b51d75d4e0c64e72731963acdd
mirzagibran221 <haqimirza21@gmail.com>
2019-12-05 20:18:09 +0700

    baru

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Blank page
+        Belajar github
         <small>it all starts here</small>
       </h1>
       <ol class="breadcrumb">
