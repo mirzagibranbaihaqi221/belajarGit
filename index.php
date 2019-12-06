857f2a58aa437b45aa2be02b0188439831315939
mirzagibran221 <haqimirza21@gmail.com>
2019-12-06 13:40:04 +0700

    index baru

--- index.php
+++ index.php
@@ -103,22 +103,7 @@
 <!-- ./wrapper -->
 
 <!-- jQuery 3 -->
-<script src="template/Adminlte2/bower_components/jquery/dist/jquery.min.js"></script>
-<!-- Bootstrap 3.3.7 -->
-<script src="template/Adminlte2/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- SlimScroll -->
-<script src="template/Adminlte2/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
-<script src="template/Adminlte2/bower_components/fastclick/lib/fastclick.js"></script>
-<!-- AdminLTE App -->
-<script src="template/Adminlte2/dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="template/Adminlte2/dist/js/demo.js"></script>
-<script>
-  $(document).ready(function () {
-    $('.sidebar-menu').tree()
-  })
-</script>
+<?php include'template/java.php';?>
 </body>
 </html>
 
